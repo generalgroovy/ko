51199b3a4c6d00a9efbc30f3a83db599974e1da4
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2026-05-30 15:50:17 +0200

    Wire song timeline into GUI

--- ko2_daw/launcher.py
+++ ko2_daw/launcher.py
@@ -35,6 +35,7 @@ def _install_gui_extensions() -> None:
     from ko2_daw.gui_midi_detection import apply_midi_detection_patch
     from ko2_daw.gui_protocol_window import apply_protocol_window_patch
     from ko2_daw.gui_scrollbars import apply_hardware_scrollbar_patch
+    from ko2_daw.gui_song_timeline import apply_song_timeline_patch
     from ko2_daw.gui_state_poll import apply_state_poll_patch
     from ko2_daw.hardware_explorer import apply_hardware_explorer_patch
 
@@ -47,6 +48,7 @@ def _install_gui_extensions() -> None:
     apply_file_explorer_window_patch(gui)
     apply_connection_guard_patch(gui)
     apply_device_main_patch(gui)
+    apply_song_timeline_patch(gui)
     apply_state_poll_patch(gui)
 
 
