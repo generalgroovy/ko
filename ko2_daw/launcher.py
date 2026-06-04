000946d6531cf8bdb2320e8876ade68bc0fd7bcc
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2026-06-04 13:27:05 +0200

    Use GUI plugin registry in launcher

--- ko2_daw/launcher.py
+++ ko2_daw/launcher.py
@@ -27,35 +27,9 @@ def _install_gui_extensions() -> None:
     """Install GUI extensions before creating the Tkinter window."""
 
     from ko2_daw import gui
-    from ko2_daw.gui_all_groups_matrix import apply_all_groups_matrix_patch
-    from ko2_daw.gui_comm_panel import apply_comm_panel_patch
-    from ko2_daw.gui_connection_guard import apply_connection_guard_patch
-    from ko2_daw.gui_detection_menu import apply_detection_menu_patch
-    from ko2_daw.gui_device_main import apply_device_main_patch
-    from ko2_daw.gui_file_explorer_window import apply_file_explorer_window_patch
-    from ko2_daw.gui_midi_detection import apply_midi_detection_patch
-    from ko2_daw.gui_protocol_window import apply_protocol_window_patch
-    from ko2_daw.gui_scrollbars import apply_hardware_scrollbar_patch
-    from ko2_daw.gui_segment_grid import apply_segment_grid_patch
-    from ko2_daw.gui_song_timeline import apply_song_timeline_patch
-    from ko2_daw.gui_state_poll import apply_state_poll_patch
-    from ko2_daw.gui_visual_stability import apply_visual_stability_patch
-    from ko2_daw.hardware_explorer import apply_hardware_explorer_patch
+    from ko2_daw.gui_plugins import install_gui_plugins
 
-    apply_protocol_window_patch(gui)
-    apply_midi_detection_patch(gui)
-    apply_detection_menu_patch(gui)
-    apply_comm_panel_patch(gui)
-    apply_hardware_explorer_patch(gui)
-    apply_hardware_scrollbar_patch(gui)
-    apply_file_explorer_window_patch(gui)
-    apply_connection_guard_patch(gui)
-    apply_device_main_patch(gui)
-    apply_song_timeline_patch(gui)
-    apply_segment_grid_patch(gui)
-    apply_all_groups_matrix_patch(gui)
-    apply_state_poll_patch(gui)
-    apply_visual_stability_patch(gui)
+    install_gui_plugins(gui)
 
 
 if __name__ == "__main__":
